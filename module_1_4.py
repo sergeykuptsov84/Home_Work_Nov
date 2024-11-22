e7567e1e8b293f7f90a4fe4919a6fb5a45ee1dee
Sergey Kuptsov <skuptsov1984@gmail.com>
2024-11-22 11:46:38 +0300

    22/11 end added

--- module_1_4.py
+++ module_1_4.py
@@ -1,8 +1,8 @@
 my_string = input('Введите строку: ')
-print(len(my_string))
-print(my_string .upper())
-print(my_string.lower())
-print(my_string.replace(' ', ''))
-print(my_string[0])
-print(my_string[-1])
-
+print(len(my_string))                            # count the characters
+print(my_string .upper())                        # uppercase
+print(my_string.lower())                         # lowercase
+print(my_string.replace(' ', ''))    # delete a space
+print(my_string[0])                              # first letter
+print(my_string[-1])                             # last letter
+end='**'
