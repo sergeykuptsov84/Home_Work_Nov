ea9c999256df5135c26ad05a46e5045c110fabd2
Sergey Kuptsov <skuptsov1984@gmail.com>
2024-11-22 13:40:19 +0300

    22/11 input added

---
+++ lists.py
@@ -0,0 +1,16 @@
+food = ['apple','perch','coconut','banana']
+print(food)
+print(food[0])
+print(food[1])
+food[3] = 'perch'
+print(food)
+food.append(True)
+print(food)
+food.extend(['string',2])
+print(food)
+food.remove('apple')
+print(food)
+print('coconut' in food, 'perch' in food)
+print(food[0:2])
+set = input('Enter')
+
