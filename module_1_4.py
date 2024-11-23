fd5d919453d4f5f96d90fd01074e91874fb36157
sergeykuptsov84 <skuptsov1984@gmail.com>
2024-11-23 08:55:47 +0300

    Delete module_1_4.py

--- module_1_4.py
+++
@@ -1,8 +0,0 @@
-my_string = input('Введите строку: ')
-print(len(my_string))                            # count the characters
-print(my_string .upper())                        # uppercase
-print(my_string.lower())                         # lowercase
-print(my_string.replace(' ', ''))    # delete a space
-print(my_string[0])                              # first letter
-print(my_string[-1])                             # last letter
-
